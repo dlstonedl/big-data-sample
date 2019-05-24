2b345f3bad40aa9f5eda85a3da9e1788e8faffcd
dlstonedl <ldeng@thoughtworks.com>
2019-05-24 23:04:16 +0800

    data set class to data frame

---
+++ src/main/scala/com/dlstone/spark/DataFrame/DataSetClassToDataFrame.scala
@@ -0,0 +1,31 @@
+package com.dlstone.spark.DataFrame
+
+import org.apache.spark.sql.{DataFrame, Dataset, SparkSession}
+
+/**
+  * DataSet to DataFrame through class
+  */
+object DataSetClassToDataFrame {
+
+  def main(args: Array[String]): Unit = {
+
+    val sparkSession = SparkSession.builder()
+      .appName("DataSetToDataFrame")
+      .master("local[*]")
+      .getOrCreate()
+
+    val lines: Dataset[String] = sparkSession.read.textFile("hdfs://localhost:9000/dataframe")
+
+    import sparkSession.implicits._
+    val personDS: Dataset[Person] = lines.map(line => {
+      val fields = line.split(",")
+      Person(fields(0).toLong, fields(1), fields(2).toInt)
+    })
+
+    val df: DataFrame = personDS.toDF()
+    df.createTempView("person")
+    val result: DataFrame = sparkSession.sql("select * from person")
+    result.show()
+
+  }
+}
