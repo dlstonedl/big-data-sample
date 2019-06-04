2b48a3e6fcca6a53f07c64d02ff1ef21d61ef835
dlstonedl <ldeng@thoughtworks.com>
2019-06-04 23:18:00 +0800

    kafka producer

--- src/main/java/com/dlstone/kafka/KafkaProducerDemo.java
+++ src/main/java/com/dlstone/kafka/KafkaProducerDemo.java
@@ -1,26 +1,32 @@
 package com.dlstone.kafka;
 
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.common.serialization.StringSerializer;
+import kafka.producer.KeyedMessage;
+import kafka.producer.Producer;
+import kafka.producer.ProducerConfig;
+import kafka.serializer.StringEncoder;
+import scala.collection.JavaConversions;
+import scala.collection.Seq;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
 
 public class KafkaProducerDemo {
 
-    public static void main(String[] args) throws ExecutionException, InterruptedException {
+    public static void main(String[] args) {
         Properties props = new Properties();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        props.put(ProducerConfig.CLIENT_ID_CONFIG, "KafkaProducerDemo");
-        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        KafkaProducer<String, String> producer = new KafkaProducer<>(props);
+        props.put("metadata.broker.list", "localhost:9092");
+        props.put("serializer.class", StringEncoder.class.getName());
 
-        ProducerRecord record = new ProducerRecord("test", "hash-key","hello bing");
-        Future result = producer.send(record);
-        System.out.println("send over!" + result.get());
+        ProducerConfig config = new ProducerConfig(props);
+        Producer<String, String> producer = new Producer<>(config);
+
+        List<KeyedMessage<String, String>> messages = new ArrayList<>();
+        KeyedMessage<String, String> msg = new KeyedMessage<>("test","hash-key" ,"hello kafka producer");
+        messages.add(msg);
+
+        Seq<KeyedMessage<String, String>> seq = JavaConversions.asScalaIterator(messages.iterator()).toSeq();
+        producer.send(seq);
+        System.out.println("send over!");
     }
 }
